34b0c5bcfbc162a7c2b2c689abfde9a2d5f5527a
yahomi-dev <yahomi.dev@gmail.com>
2021-11-23 22:20:32 +0900

    learned enum

---
+++ src/enum.ts
@@ -0,0 +1,37 @@
+export {};
+
+enum Months {
+  January = 1,
+  Februrary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log({});
+console.log(Months.Februrary);
+console.log(Months.December);
+
+enum COLORS {
+  RED = '#FF0000',
+  EHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  BLACK = '#000000',
+  // YELLOW = '#FFFF00',
+}
+
+let green = COLORS.GREEN;
+console.log({ green });
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080',
+}
