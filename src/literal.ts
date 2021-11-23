a029eb41848a502061065faeb1c1fc9d055f00ae
yahomi-dev <yahomi.dev@gmail.com>
2021-11-23 21:43:03 +0900

    learned literal

---
+++ src/literal.ts
@@ -0,0 +1,9 @@
+export {};
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土';
+dayOfTheWeek = '月';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 9 | 10 | 11 | 12;
+month = 12;
+
+// month = 13;
