c988e4c985094274ad6b157456ed8744b5f6c6b8
yahomi-dev <yahomi.dev@gmail.com>
2021-11-29 11:40:01 +0900

    learned compatibility

---
+++ src/advanced-type/compatibility.ts
@@ -0,0 +1,42 @@
+export {};
+
+let fooCompatible: any;
+let barCompatible: string = 'TypeScript';
+
+console.log(typeof fooCompatible);
+
+fooCompatible = barCompatible;
+
+console.log(typeof fooCompatible);
+
+let fooIncompatible: string;
+let barIncompatible: number = 1;
+
+// fooIncompatible = barIncompatible;
+//
+
+let fooString: string;
+let barString: string = 'String';
+
+fooString = barString;
+
+let foostringLiteral: 'fooStringLiteral' = 'fooStringLiteral';
+fooString = foostringLiteral;
+
+let fooNumber: number;
+let fooNumberLiteral: 1976 = 1976;
+fooNumber = fooNumberLiteral;
+
+interface Animal {
+  age: number;
+  name: string;
+}
+
+class Person {
+  constructor(public age: number, public name: string) {}
+}
+
+let me: Animal;
+me = new Person(43, 'Ham');
+
+console.log(me);
