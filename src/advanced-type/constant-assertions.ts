87cea39fb1c048ce32b9faa4d9a26d40a28db8e1
yahomi-dev <yahomi.dev@gmail.com>
2021-12-01 19:50:09 +0900

    learned as const

---
+++ src/advanced-type/constant-assertions.ts
@@ -0,0 +1,17 @@
+export {};
+
+let name = 'Atsushi';
+
+name = 'Ham';
+
+let nickName = 'Ham' as const;
+
+nickName = 'Ham';
+
+let profile = {
+  name: 'Atsushi',
+  height: 178,
+} as const;
+
+profile.name = 'Atsushi';
+profile.height = 178;
